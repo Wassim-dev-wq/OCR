6465639bd695f9f11f08d2107074827d20a6f55b
wassim <wbenguerine@gmail.com>
2023-02-09 22:21:15 +0100

    init proj

---
+++ main.py
@@ -0,0 +1,37 @@
+import cv2
+import pyocr
+import pyocr.builders
+import pandas as pd
+import pytesseract
+# Set the path to tesseract
+#pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe" # for windows
+# OR
+pytesseract.pytesseract.tesseract_cmd = r"/usr/bin/tesseract" # for Linux
+
+# Load image file
+image = cv2.imread('table.png')
+if image is None:
+    print("Error: Image not found or loaded")
+else:
+    # Pre-process the image
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Perform OCR
+text = pytesseract.image_to_string(gray)
+
+# Split the text by line
+lines = text.split('\n')
+
+# Define the delimiter for the data
+delimiter = ' '
+
+# Extract the data
+data = []
+for line in lines:
+    data.append(line.split(delimiter))
+
+# Convert the extracted data to a pandas DataFrame
+df = pd.DataFrame(data)
+
+# Save the extracted data to a CSV file
+df.to_csv('table.csv', index=False)
